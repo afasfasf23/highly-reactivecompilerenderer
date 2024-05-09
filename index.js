9fb12a9e529dd3e02fa03c2eb69374ce82050305
afasfasf23 <js1hamran03@hotmail.com>
2024-05-09 07:37:40 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-function isAnagram(s, t) {
-  if (s.length !== t.length) return false;
-  const count = Array(26).fill(0);
-  for (let i = 0; i < s.length; i++) {
-    count[s.charCodeAt(i) - 97]++;
-    count[t.charCodeAt(i) - 97]--;
-  }
-  return count.every((c) => c === 0);
+const number = 10;
+if (number % 2 === 0) {
+  console.log("Even number");
+} else {
+  console.log("Odd number");
 }
