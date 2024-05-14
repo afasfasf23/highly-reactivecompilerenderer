4e4867dd5365d078687f07d1bb4cabe07df90906
afasfasf23 <js1hamran03@hotmail.com>
2024-05-15 00:48:22 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+function kthSmallest(matrix, k) {
+  const flatten = matrix.reduce((acc, row) => acc.concat(row), []);
+  flatten.sort((a, b) => a - b);
+  return flatten[k - 1];
+}
